09fc76a97e0f5baee0e6d43c6957ce4f3870b754
lizhen <uowyuchen@gmail.com>
2020-10-08 19:58:10 -0400

    Header & Footer & Bootstrap-Watch & React-Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
-  return <>This is Jay shop</>
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>This is Jay shop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App
