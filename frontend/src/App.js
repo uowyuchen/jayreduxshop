e6aaab9460b1af95e41f20b1ab18c492ea659c37
lizhen <uowyuchen@gmail.com>
2020-10-22 17:10:54 -0400

    Implementing React Router & Link & LinkContainer from react-router-bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screen/HomeScreen'
+import ProductScreen from './screen/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <main className='py-3'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
