db73bc7db4348104b3e91e09234a0f75a4c403ac
lizhen <uowyuchen@gmail.com>
2020-10-23 17:25:37 -0400

    Mongoose & Connect to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+// connect DB
+connectDB()
 
 const app = express()
 
@@ -24,6 +29,7 @@ const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(
-    `server running on port ${process.env.NODE_ENV} mode on port ${PORT}`
+    `server running on port ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 )
