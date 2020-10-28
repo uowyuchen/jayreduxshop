dacca5487f37579686ace44901d797f570c9a046
lizhen <uowyuchen@gmail.com>
2020-10-27 23:41:08 -0400

    Single Product Reducer & Action - 5.5 is same as 5.2

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,21 +2,48 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
 
-const INITIAL_STATE = {
+// Reducer of all products
+const INITIAL_STATE_PRODUCTS = {
   products: [],
   loading: null,
   error: null,
 }
 
-export const productListReducer = (state = INITIAL_STATE, action) => {
+export const productListReducer = (state = INITIAL_STATE_PRODUCTS, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
-      return { loading: true }
+      return { loading: true, products: [] }
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload }
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+// Reducer of single product
+const INITIAL_STATE_PRODUCT = {
+  product: {},
+  loading: null,
+  error: null,
+}
+
+export const productDetailsReducer = (
+  state = INITIAL_STATE_PRODUCT,
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, product: {} }
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload }
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
